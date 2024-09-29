e5db2228c595abbd2d6008cfb65b91618296f7f5
Mamonha <mamonha302@gmail.com>
2024-09-29 00:09:34 -0300

    Using chi to create initial route

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"wpp-integration/controllers"
+
+	"github.com/go-chi/chi"
+)
+
 func main() {
 
+	r := chi.NewRouter()
+
+	r.Post("/api/consultas", controllers.HandleConsulta)
+
+	fmt.Println("Servidor rodando na porta 8080")
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
