c16e6217e72d48c96758af255585c419416517db
Mamonha <mamonha302@gmail.com>
2024-09-29 19:13:19 -0300

    Defining webook route

--- main.go
+++ main.go
@@ -5,16 +5,23 @@ import (
 	"log"
 	"net/http"
 	"wpp-integration/controllers"
+	"wpp-integration/services"
 
 	"github.com/go-chi/chi"
 )
 
 func main() {
 
+	dsn := "root:root@tcp(127.0.0.1:3306)/clinifono?charset=utf8mb4&parseTime=True&loc=Local"
+	if err := services.InitDB(dsn); err != nil {
+		log.Fatalf("Erro ao inicializar o banco de dados: %v", err)
+	}
+
 	r := chi.NewRouter()
 
 	r.Post("/api/consultas", controllers.HandleConsulta)
 
-	fmt.Println("Servidor rodando na porta 8080")
+	r.Post("/api/webhook", controllers.Webhook)
+	fmt.Println("Servidor rodando na porta 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
